5a3ed3c2c98b7d7163dcc4c59c7d7a52a73b6756
Dean Mao <deanmao@gmail.com>
2013-11-14 14:01:58 -0800

    set cookie

--- api/account.js
+++ api/account.js
@@ -1,5 +1,4 @@
 (function() {
-
   module.exports = function(Reddit) {
     Reddit.prototype.clearSessions = function(modhash, password, url, callback) {
       var options, params,
@@ -34,6 +33,9 @@
         return callback();
       });
     };
+    Reddit.prototype.setCookie = function(cookie) {
+      return this._agent.jar.setCookies(["reddit_session=" + cookie + "; Domain=reddit.com; Path=/; HttpOnly"]);
+    };
     Reddit.prototype.login = function(username, password, callback) {
       var options, params,
         _this = this;
@@ -44,13 +46,67 @@
         passwd: password,
         rem: false
       };
-      return this._post('/api/login', options, params, function(error, res) {
-        var _ref, _ref1, _ref2, _ref3, _ref4;
-        _this._agent.jar.setCookies(["reddit_session=" + ((_ref = res.body) != null ? (_ref1 = _ref.json) != null ? (_ref2 = _ref1.data) != null ? _ref2.cookie : void 0 : void 0 : void 0) + "; Domain=reddit.com; Path=/; HttpOnly"]);
-        if (error != null) {
-          return callback(error);
+      this._post('/api/login', options, params, function(error, res) {
+        var data, _ref, _ref1;
+        return data = (_ref = res.body) != null ? (_ref1 = _ref.json) != null ? _ref1.data : void 0 : void 0;
+      });
+      this._agent.jar.setCookies(["reddit_session=" + (typeof data !== "undefined" && data !== null ? data.cookie : void 0) + "; Domain=reddit.com; Path=/; HttpOnly"]);
+      if (typeof error !== "undefined" && error !== null) {
+        return callback(error);
+      }
+      return callback(null, typeof data !== "undefined" && data !== null ? data.modhash : void 0, typeof data !== "undefined" && data !== null ? data.cookie : void 0);
+    };
+    Reddit.prototype.oAuthAuthorize = function(clientId, clientSecret, state, code, scope, callback) {
+      var details, options,
+        _this = this;
+      if (scope == null) {
+        scope = ['identity'];
+      }
+      if (typeof scope === 'function') {
+        callback = scope;
+        scope = ['identity'];
+      }
+      options = {
+        state: state,
+        scope: scope.join(','),
+        client_id: 'tMsPeTkhps5_tg',
+        redirect_uri: 'http://reddichat.com/reddit/oauth',
+        code: code,
+        grant_type: 'authorization_code'
+      };
+      details = {
+        name: "reddit OAuth authorization",
+        options: options
+      };
+      return this._enqueue(details, function(finished) {
+        return _this._agent.post("https://" + clientId + ":" + clientSecret + "@ssl.reddit.com/api/v1/access_token").set('Content-Type', 'application/x-www-form-urlencoded').set('User-Agent', _this._userAgent).send(options).end(function(res) {
+          if (res.status === 200) {
+            callback(null, res.body);
+          } else {
+            callback(new Error(JSON.stringify(details)));
+          }
+          return finished();
+        });
+      });
+    };
+    Reddit.prototype.oAuthMe = function(token, callback) {
+      var details,
+        _this = this;
+      details = {
+        name: "reddit OAuth Me",
+        options: {
+          token: token
         }
-        return callback(null, (_ref3 = res.body.json) != null ? (_ref4 = _ref3.data) != null ? _ref4.modhash : void 0 : void 0);
+      };
+      return this._enqueue(details, function(finished) {
+        return _this._agent.get('https://oauth.reddit.com/api/v1/me').set('Authorization', "bearer " + token).set('User-Agent', _this._userAgent).end(function(res) {
+          if (res.status === 200) {
+            callback(null, res.body);
+          } else {
+            callback(new Error(JSON.stringify(details)));
+          }
+          return finished();
+        });
       });
     };
     Reddit.prototype.me = function(callback) {
--- src/api/account.coffee
+++ src/api/account.coffee
@@ -2,63 +2,68 @@
 module.exports = (Reddit) ->
 
 	Reddit::clearSessions = (modhash, password, url, callback) ->
-		
+
 		options =
 			curpass: password
 			dest: url
 			uh: modhash
-		
+
 		params = Object.keys options
-		
+
 		@_post '/api/clear_sessions', options, params, (error, res) =>
-			
+
 			return callback error if error?
-			
+
 			callback()
-				
+
 	Reddit::deleteUser = (username, password, modhash, callback) ->
-		
+
 		options =
 			confirm: true
 			passwd: password
 			uh: modhash
 			user: username
-		
+
 		params = Object.keys options
-		
+
 		@_post '/api/delete_user', options, params, (error, res) =>
-			
+
 			return callback error if error?
-			
+
 			callback()
-				
+
+	Reddit::setCookie = (cookie) ->
+		@_agent.jar.setCookies([
+			"reddit_session=#{cookie}; Domain=reddit.com; Path=/; HttpOnly"
+		])
+
 	Reddit::login = (username, password, callback) ->
-		
+
 		params = ['user', 'passwd']
-		
+
 		options =
 			api_type: 'json'
 			user: username
 			passwd: password
 			rem: false
-		
+
 		@_post '/api/login', options, params, (error, res) =>
-			
+		  data = res.body?.json?.data
 			@_agent.jar.setCookies([
-				"reddit_session=#{res.body?.json?.data?.cookie}; Domain=reddit.com; Path=/; HttpOnly"
+				"reddit_session=#{data?.cookie}; Domain=reddit.com; Path=/; HttpOnly"
 			])
-			
+
 			return callback error if error?
-		
-			callback null, res.body.json?.data?.modhash
-	
+
+			callback null, data?.modhash, data?.cookie
+
 	Reddit::oAuthAuthorize = (clientId, clientSecret, state, code, scope = ['identity'], callback) ->
-		
+
 		if typeof scope is 'function'
-			
+
 			callback = scope
 			scope = ['identity']
-	
+
 		options =
 			state: state
 			scope: scope.join ','
@@ -66,66 +71,66 @@ module.exports = (Reddit) ->
 			redirect_uri: 'http://reddichat.com/reddit/oauth'
 			code: code
 			grant_type: 'authorization_code'
-		
+
 		details =
 			name: "reddit OAuth authorization"
 			options: options
-		
+
 		@_enqueue details, (finished) =>
-			
+
 			@_agent
 				.post("https://#{clientId}:#{clientSecret}@ssl.reddit.com/api/v1/access_token")
 				.set('Content-Type', 'application/x-www-form-urlencoded')
 				.set('User-Agent', @_userAgent)
 				.send(options)
 				.end (res) ->
-					
+
 					if res.status is 200
-						
+
 						callback null, res.body
-						
+
 					else
-						
+
 						callback new Error JSON.stringify details
-						
-					
-					finished() 
-		
+
+
+					finished()
+
 	Reddit::oAuthMe = (token, callback) ->
-		
+
 		details =
 			name: "reddit OAuth Me"
 			options:
 				token: token
-		
+
 		@_enqueue details, (finished) =>
-			
+
 			@_agent
 				.get('https://oauth.reddit.com/api/v1/me')
 				.set('Authorization', "bearer #{token}")
 				.set('User-Agent', @_userAgent)
 				.end (res) ->
-					
+
 					if res.status is 200
-						
+
 						callback null, res.body
-						
+
 					else
-						
+
 						callback new Error JSON.stringify details
-						
-					finished() 
-					
+
+					finished()
+
 	Reddit::me = (callback) ->
-				
+
 		@_get '/api/me.json', (error, res) ->
-		
+
 			return callback error if error?
-			
+
 			callback null, res.body.data
 
 	Reddit::update = (password, email, newPassword, modhash, callback) ->
-		
+
 		options =
 			curpass: password
 			email: email
@@ -133,12 +138,12 @@ module.exports = (Reddit) ->
 			uh: modhash
 			verify: true
 			verpass: newPassword
-		
+
 		params = Object.keys options
-		
+
 		@_post '/api/update', options, params, (error, res) ->
-		
+
 			return callback error if error?
-			
+
 			callback()
 
